bf65e8c73eccf59bc114eec85c6b4791ab3ba42b
Qingyang <lemonde@qq.com>
2016-02-24 16:54:36 +0800

    -ios h5

---
+++ h5/ios/pages/BaseWeb/H5Constant.h
@@ -0,0 +1,15 @@
+//
+//  H5Constant.h
+//  H5
+//
+//  Created by Qingyang on 16/2/24.
+//  Copyright © 2016年 Qingyang. All rights reserved.
+//
+
+#ifndef H5Constant_h
+#define H5Constant_h
+
+#define NATIVE_FUNCTION_DEMO @"NATIVE_FUNCTION_DEMO"
+#define NATIVE_FUNCTION_OPENCAMERA @"NATIVE_FUNCTION_OPENCAMERA"
+#define NATIVE_FUNCTION_OPENGPS @"NATIVE_FUNCTION_OPENGPS"
+#endif /* H5Constant_h */

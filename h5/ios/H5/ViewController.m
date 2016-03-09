004d82303f03c26ad3a52df8d56335f2eca3d9a1
zhchen <zhchen@lhtxdeMac-mini.local>
2016-03-09 17:12:43 +0800

    嵌入H5播放视频。横竖屏问题

--- h5/ios/H5/ViewController.m
+++ h5/ios/H5/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 #import "H5VC.h"
 #import "H5newVC.h"
+#import "H5PlayerVC.h"
 //#import "ViewController2.h"
 @interface ViewController ()
 
@@ -32,7 +33,25 @@
 }
 - (IBAction)H5newAction:(id)sender {
     H5newVC *vc = [[H5newVC alloc] initWithNibName:@"H5newVC" bundle:nil];
+    [self presentViewController:vc animated:YES completion:nil];
+    //[self.navigationController pushViewController:vc animated:YES];
+}
+- (IBAction)H5PlayerVCAction:(id)sender {
+    H5PlayerVC *vc = [[H5PlayerVC alloc] initWithNibName:@"H5PlayerVC" bundle:nil];
     [self.navigationController pushViewController:vc animated:YES];
 }
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
 
+-(BOOL)shouldAutorotate
+{
+    return NO;
+}
+
+-(UIInterfaceOrientationMask)supportedInterfaceOrientations
+{
+    return UIInterfaceOrientationMaskPortrait;
+}
 @end
--- h5/ios/views/pages/H5PlayerVC.m
+++ h5/ios/views/pages/H5PlayerVC.m
@@ -1,51 +1,26 @@
 //
-//  H5newVC.m
+//  H5PlayerVC.m
 //  H5
 //
-//  Created by zhchen on 16/3/7.
+//  Created by zhchen on 16/3/9.
 //  Copyright © 2016年 Qingyang. All rights reserved.
 //
 
-#import "H5newVC.h"
-
-@interface H5newVC ()<UIWebViewDelegate,WKNavigationDelegate,WKUIDelegate>
+#import "H5PlayerVC.h"
+@interface H5PlayerVC ()<UIWebViewDelegate,WKNavigationDelegate,WKUIDelegate>
 
 @end
 
-@implementation H5newVC
+@implementation H5PlayerVC
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    UIBarButtonItem *left = [[UIBarButtonItem alloc] initWithTitle:@"返回" style:UIBarButtonItemStylePlain target:self action:@selector(leftAction:)];
-    self.navigationItem.leftBarButtonItem = left;
-//     Do any additional setup after loading the view from its nib.
+    // Do any additional setup after loading the view from its nib.
 }
-//- (void)leftAction:(UIBarButtonItem *)sender
-//{
-//    [self.navigationController popViewControllerAnimated:YES];
-//    if ([[UIDevice currentDevice] respondsToSelector:@selector(setOrientation:)]) {
-//        SEL selector = NSSelectorFromString(@"setOrientation:");
-//        NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:[UIDevice instanceMethodSignatureForSelector:selector]];
-//        [invocation setSelector:selector];
-//        [invocation setTarget:[UIDevice currentDevice]];
-//        int val = UIInterfaceOrientationPortrait;
-//        [invocation setArgument:&val atIndex:2];
-//        [invocation invoke];
-//    }
-//}
+
 
 - (void)WebUIInit
 {
-//    if ([[UIDevice currentDevice] respondsToSelector:@selector(setOrientation:)]) {
-//        SEL selector = NSSelectorFromString(@"setOrientation:");
-//        NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:[UIDevice instanceMethodSignatureForSelector:selector]];
-//        [invocation setSelector:selector];
-//        [invocation setTarget:[UIDevice currentDevice]];
-//        int val = UIInterfaceOrientationLandscapeRight;
-//        [invocation setArgument:&val atIndex:2];
-//        [invocation invoke];
-//    }
-    
     
     self.edgesForExtendedLayout = UIRectEdgeNone;
     if (self.webViewMain == nil) {
@@ -54,7 +29,7 @@
         self.webViewMain.navigationDelegate = self;
         self.webViewMain.UIDelegate = self;
         self.webViewMain.allowsBackForwardNavigationGestures = YES;
-         self.webViewMain.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
+        self.webViewMain.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
         [self.view addSubview:self.webViewMain];
         UIProgressView *progressView = [[UIProgressView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 0)];
         progressView.tintColor = [UIColor orangeColor];
@@ -66,7 +41,7 @@
         button.backgroundColor = [UIColor redColor];
         [self.webViewMain addSubview:button];
         [button addTarget:self action:@selector(ButtonAction:) forControlEvents:UIControlEventTouchUpInside];
-        [self.webViewMain loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://www.baidu.com"]]];
+        [self.webViewMain loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://172.16.7.140/MediaTest/test/test.html"]]];
         [self.view insertSubview:self.webViewMain belowSubview:self.progressView];
         
         
@@ -75,23 +50,33 @@
     [self.webViewMain addObserver:self forKeyPath:@"estimatedProgress" options:NSKeyValueObservingOptionNew context:NULL];
     [self.webViewMain addObserver:self forKeyPath:@"title" options:NSKeyValueObservingOptionNew context:NULL];
 }
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
-}
 
--(UIInterfaceOrientationMask)supportedInterfaceOrientations
-{
-    return UIInterfaceOrientationMaskAllButUpsideDown;
-}
-
--(BOOL)shouldAutorotate
-{
-    return YES;
-}
 - (void)ButtonAction:(UIButton *)sender
 {
-    [self dismissViewControllerAnimated:YES completion:nil];
+    
+    sender.selected = !sender.selected;
+    if (sender.selected) {
+        if ([[UIDevice currentDevice] respondsToSelector:@selector(setOrientation:)]) {
+            SEL selector = NSSelectorFromString(@"setOrientation:");
+            NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:[UIDevice instanceMethodSignatureForSelector:selector]];
+            [invocation setSelector:selector];
+            [invocation setTarget:[UIDevice currentDevice]];
+            int val = UIInterfaceOrientationLandscapeRight;
+            [invocation setArgument:&val atIndex:2];
+            [invocation invoke];
+        }
+    }else{
+    if ([[UIDevice currentDevice] respondsToSelector:@selector(setOrientation:)]) {
+                SEL selector = NSSelectorFromString(@"setOrientation:");
+                NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:[UIDevice instanceMethodSignatureForSelector:selector]];
+                [invocation setSelector:selector];
+                [invocation setTarget:[UIDevice currentDevice]];
+                int val = UIInterfaceOrientationPortrait;
+                [invocation setArgument:&val atIndex:2];
+                [invocation invoke];
+            }
+    }
+    //[self preferredInterfaceOrientationForPresentation];
 }
 
 - (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation
@@ -127,12 +112,20 @@
     [self.webViewMain setNavigationDelegate:nil];
     [self.webViewMain setUIDelegate:nil];
 }
-
-- (UIInterfaceOrientation)preferredInterfaceOrientationForPresentation
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-    return UIInterfaceOrientationLandscapeRight;
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
 }
 
+-(UIInterfaceOrientationMask)supportedInterfaceOrientations
+{
+    return UIInterfaceOrientationMaskAllButUpsideDown;
+}
+
+-(BOOL)shouldAutorotate
+{
+    return YES;
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.

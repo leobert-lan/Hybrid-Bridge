7cd4638745dd8059c528e89afd46ee10aa629e45
Qingyang <lemonde@qq.com>
2016-12-05 11:48:28 +0800

    验证auth

--- h5/iOS_Hybrid/iOS_Hybrid_App/view/API_WEB/WebAuthAPI.h
+++ h5/iOS_Hybrid/iOS_Hybrid_App/view/API_WEB/WebAuthAPI.h
@@ -9,6 +9,7 @@
 #import <Foundation/Foundation.h>
 
 @interface WebAuthAPI : NSObject
+//这里是两种写法，如果data有具体model，这里可以注明，便于使用
 //+ (void)AuthListener:(WKWebViewJavascriptBridge *)bridge handler:(void (^)(id bridge, id data, NetError *err, WVJBResponseCallback responseCallback))handler;
 + (void)AuthInfoListener:(WKWebViewJavascriptBridge *)bridge handler:(H5Listener)handler;
 @end
